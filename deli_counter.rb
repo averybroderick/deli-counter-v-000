77c811f235fb67ce8b2ccf370bdfc00ff37deada
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-23 15:01:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def take_a_number(arr, customer)
   puts "Welcome, #{customer}. You are number #{arr.size} in line."
 end
 
-def now_serving
+def now_serving(arr)
   if arr.size == 0
     "There is nobody waiting to be served!"
   else
