26c65869776a7695d8734878624b2a3c11246605
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-23 14:50:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 def line(arr)
-  if arr.size > 0
-    puts arr
+  if arr.size
+    arr.each_with_index do |customer, index|
+    puts "The line is currently: #{index+1}. #{customer}"
   else
     puts "The line is currently empty."
   end
